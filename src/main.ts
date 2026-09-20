5e7508777194976ae134bdd1416dfefbcbb4cf36
fernandoDev23 <feroficial.dev@gmail.com>
2026-09-20 13:23:23 -0300

    checkout finalizado com sucesso.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  app.enableCors();
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
-  const port = process.env.PORT ?? 3003
+  const config = new DocumentBuilder()
+    .setTitle('Checkout Service')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3003;
   await app.listen(port);
+
+  console.log(`🛒 Checkout Service running on port ${port}`);
 }
+
 bootstrap();
